876739e46dd13a209443b45a3283c0b54cad67bb
Owen Mariani <mariani.owen@gmail.com>
2024-10-03 13:15:32 -0400

    optimized space for #198

--- Completed/198.py
+++ Completed/198.py
@@ -1,22 +1,21 @@
-class Solution(object):
-    def rob(self, nums):
+def rob(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
 
-        # explanation: simple DP
-        
         n = len(nums)
-        
+
         if n < 3:
             return max(nums)
+
         
-        M = [0] * n
-        M[0] = nums[0]
-        M[1] = max(nums[0], nums[1])
+        prev2 = nums[0]
+        prev1 = max(nums[0], nums[1])
+        temp = 0
 
         for i in range(2, n):
-            M[i] = max(M[i-1], M[i-2] + nums[i])
-
-        return M[-1]
+            temp = prev1
+            prev1 = max(prev1, prev2 + nums[i])
+            prev2 = temp
+        return prev1
